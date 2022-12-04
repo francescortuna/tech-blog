d39f69aa2b0ed7068a876c19e8f82ddca737d3d7
Frances Cortuna <francescortuna@gmail.com>
2022-12-04 01:22:15 -0500

    Updated model/index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,23 @@
-// User has many blog posts -> One to many
-// User has many comments -> One to many
-// Blog posts have many comments -> One to many
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+// User has many blog posts -> One to many
+User.hasMany(Post, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+// User has many comments -> One to many
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+// Blog posts have many comments -> One to many
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE"
+});
+
+module.exports = { User, Post, Comment }; 
