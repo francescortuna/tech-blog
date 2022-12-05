d7b018249bb7b08ae13c00b2305c75b1630ab728
Frances Cortuna <francescortuna@gmail.com>
2022-12-04 21:49:36 -0500

    Added user routes

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const UserRoutes = require("./userRoutes");
+const PostRoutes = require("./postRoutes");
+const CommentRoutes = require("./commentRoutes");
+
+router.use("/users", UserRoutes);
+// router.use("/posts", PostRoutes);
+// router.use("/comments", CommentRoutes);
+
+module.exports = router;
