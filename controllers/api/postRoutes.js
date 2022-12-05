9d347a3785e759472c3b214bc489b48b2830d1a5
Frances Cortuna <francescortuna@gmail.com>
2022-12-04 23:13:12 -0500

    Added functionality to update post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -4,7 +4,7 @@ const { User, Post, Comment } = require("../../models");
 // "/api/posts"
 
 // create a post
-router.post("/", async (req,res) => {
+router.post("/", async (req, res) => {
   try {
     const post = await Post.create({
       ...req.body,
@@ -17,18 +17,39 @@ router.post("/", async (req,res) => {
   }
 });
 
-// delete a post
-router.delete("/:id", async (req,res) => {
+// update a post based on its id
+router.put("/:id", async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete a post based on its id
+router.delete("/:id", async (req, res) => {
   try {
     const postData = await Post.destroy({
       where: {
         id: req.params.id,
         // user_id: req.session.user_id
-      }
+      },
     });
 
-    if(!postData) {
-      res.status(404).json("No post found with this ID!")
+    if (!postData) {
+      res.status(404).json("No post found with this ID!");
       return;
     }
 
@@ -36,6 +57,6 @@ router.delete("/:id", async (req,res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
 module.exports = router;
