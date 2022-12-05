71250ce966a0050280dbf0c55e01509ea29bc5e9
Frances Cortuna <francescortuna@gmail.com>
2022-12-04 23:07:23 -0500

    Added functionality to delete post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -17,4 +17,25 @@ router.post("/", async (req,res) => {
   }
 });
 
+// delete a post
+router.delete("/:id", async (req,res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        // user_id: req.session.user_id
+      }
+    });
+
+    if(!postData) {
+      res.status(404).json("No post found with this ID!")
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 module.exports = router;
